ec8918017e10da0368baab6ebacef18461036b5c
Jan-Philipp Litza <janphilipp@litza.de>
2012-11-22 10:26:53 +0100

    added closing script tag in sample.php

--- sample.php
+++ sample.php
@@ -65,7 +65,7 @@
       padding: 5px;*/
     }
   </style>
-  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js" type="text/javascript">
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js" type="text/javascript"></script>
 </head>
 <body>
 <ul class="calendar">
